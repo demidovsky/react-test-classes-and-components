29e4fef693af62ab99ced409c5ec8e95644bd08d
Dmitry Demidovsky <d.demidovsky@gmail.com>
2023-03-12 09:14:54 +0100

    Testing for components

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'node', // or 'jsdom'
+  moduleNameMapper: {
+    "\\.(svg|css)$": "<rootDir>/__mocks__/fileMock.js"
+  },
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,10 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 import App from './App';
 
 test('renders learn react link', () => {
